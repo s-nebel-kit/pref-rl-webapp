bfe19013c3d7f2828d80f02f8f6e8d04d837fa81
Sascha <sascha.nebel@student.kit.edu>
2021-10-23 16:29:35 +0200

    Sync commit

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Preference(models.Model):
+    created_timestamp = models.DateTimeField('created at', auto_now_add=True, db_index=True)
+    updated_timestamp = models.DateTimeField('last updated at', auto_now=True, db_index=True)
 
+    video_url_left = models.TextField('video url left', db_index=True)
+    video_url_right = models.TextField('video url right', db_index=True)
+
+    shown_to_human_timestamp = models.DateTimeField('shown to human at', db_index=True, blank=True, null=True)
+    response_timestamp= models.DateTimeField('response given at', db_index=True, blank=True, null=True)
+
+    label = models.IntegerField('preference label', db_index=True, blank=True, null=True)
+
+    
