1abe1a25356047e66b707f1fa7ec72a49ee0faf6
sascha <sascha.nebel@student.kit.edu>
2021-11-07 00:46:10 +0100

    Adjusted models

--- preferences/models.py
+++ preferences/models.py
@@ -7,14 +7,15 @@ class Preference(models.Model):
     created_timestamp = models.DateTimeField('created at', auto_now_add=True, db_index=True)
     updated_timestamp = models.DateTimeField('last updated at', auto_now=True, db_index=True)
 
-    video_url_left = models.TextField('video url left', db_index=True)
-    video_url_right = models.TextField('video url right', db_index=True)
-
     shown_to_human_timestamp = models.DateTimeField('shown to human at', db_index=True, blank=True, null=True)
     response_timestamp= models.DateTimeField('response given at', db_index=True, blank=True, null=True)
 
-    label = models.DecimalField('preference label', db_index=True, blank=True, null=True)
+    label = models.DecimalField('preference label', max_digits=2, decimal_places=1, db_index=True, blank=True, null=True)
 
     def __str__(self) -> str:
         return str(self.id)
+
+    @property
+    def video_file_path_left(self) -> str:
+        return '/{}/{}-left'.format(self.id, self.uuid)
     
