0e2cd5d2189be9ed98ca4b4ba73e26959dec84f8
sascha <sascha.nebel@student.kit.edu>
2021-11-09 17:00:57 +0100

    Added submit functionality and next query redirect

--- preferences/urls.py
+++ preferences/urls.py
@@ -6,6 +6,8 @@ from preferences import views
 
 urlpatterns =[
     path('', views.index, name='index'),
+    path('next', views.next_query, name='next'),
+    path('next/', views.next_query, name='next'),
     path('<uuid:query_id>', views.query, name='query'),
     path('<uuid:query_id>/', views.query, name='query'),
     url(r'^(?P<path>.*)$', serve, {'document_root': '/home/sascha/BA/Pref-RL/Pref-RL/videofiles'})
--- preferences/views.py
+++ preferences/views.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Preference
 
@@ -12,10 +12,30 @@ def index(request):
     return render(request, 'preferences/index.html', context)
 
 
+def next_query(request):
+    top_query = Preference.objects.filter(label__isnull=True).first()
+    if top_query is not None:
+        return redirect('query', query_id=top_query.uuid)
+    else:
+        return redirect('index')
+
+
 def query(request, query_id):
 
     query = get_object_or_404(Preference, uuid=query_id)
-    #if request.method == 'POST':
+    if request.method == 'POST' and (label := request.POST['action']) is not None:
+        if label == 'Left':
+            query.label = 1
+        elif label =='Right':
+            query.label = 0
+        elif label =='Indifferent':
+            query.label = .5
+        else:
+            #TODO implement proper skip functionality
+            pass
+        query.full_clean()
+        query.save()
+        return redirect('next')
 
     context = {
         'query': query,
